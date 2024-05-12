741b3f0aef0e97b3ce24186f6f40d34b1d05561c
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-05-12 10:35:50 -0400

    tree class

--- src/index.js
+++ src/index.js
@@ -5,3 +5,15 @@ class Node {
     this.right = null;
   }
 }
+
+class Tree {
+  constructor(arr) {
+    this.treeArr = arr.sort();
+    this.root = null;
+  }
+
+  buildTree;
+}
+
+const tree = new Tree([3, 5, 8, 6, 2, 7, 1, 4]);
+console.log(tree.treeArr);
